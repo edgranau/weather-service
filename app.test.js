8d6de6b4382ba49f2ddf5382080816971e4e4214
Edwin Granados <edgranau@users.noreply.github.com>
2024-01-31 17:40:25 +1100

    style: 🎨 cleanup

--- app.test.js
+++ app.test.js
@@ -1,11 +1,12 @@
-import { app } from './app.js'
-import request from "supertest";
+import { app } from './app.js';
+import { Redis } from 'ioredis-mock';
+import request from 'supertest';
 
 jest.mock('./cacheService.js', () => jest.requireActual('ioredis-mock'));
 
 describe('/health', () => {
-  test("it returns 200 OK", async () => {
-    const resp = await request(app).get("/health");
+  test('it returns 200 OK', async () => {
+    const resp = await request(app).get('/health');
 
     expect(resp.status).toBe(200);
   });
